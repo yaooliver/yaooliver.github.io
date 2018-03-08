55819e973fa86bcdea6bf5a25ef53956381d1a30
yaooliver <603696621@qq.com>
2018-03-08 18:18:54 +0800

    yaooliver

--- 九宫格.js
+++ 九宫格.js
@@ -1,17 +1,3 @@
-function chioce() {
-    var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    var return_array = new Array();
-    for (var i = 0; i < 3; i++) {
-        if (arr.length > 0) {
-            var Index = Math.floor(Math.random() * arr.length);
-            return_array[i] = arr[Index];
-            arr.splice(Index, 1);
-        } else {
-            break;
-        }
-    }
-    return return_array;
-}
 
 function change() {
     var r = Math.floor(Math.random() * 256);
@@ -22,16 +8,41 @@ function change() {
 var start = document.getElementById('start');
 var restart = document.getElementById('restart');
 var boxChioce = document.getElementsByClassName('box');
-
-start.onclick = function () {
-    timer=setInterval(function () {
+var timer;
+function clickchange() {
         for (var m = 0; m < 9; m++) {
             boxChioce[m].style.background = "orange";
         }
-        for (var n = 0; n < 3; n++) {
-            boxChioce[chioce()[n] - 1].style.background = change();
+    function chioce() {
+        var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        var return_array = new Array();
+        for (var i = 0; i < 3; i++) {
+            if (arr.length > 0) {
+                var Index = Math.floor(Math.random() * arr.length);
+                return_array[i] = arr[Index];
+                arr.splice(Index, 1);
+            } else {
+                break;
+            }
         }
-    }, 1000);
+        return return_array;
+        
+    }
+    console.log(chioce());
+    
+    var f = chioce();
+        for (var n = 0; n < 3; n++) {
+            boxChioce[f[n]].style.background = change();
+        }
+}
+// console.log(chioce());
+start.onclick = function () {
+    clearInterval(timer);
+    for (var m = 0; m < 9; m++) {
+        boxChioce[m].style.background = "orange";
+    }
+     timer = setInterval("clickchange()", 1000);
+    
 };
 restart.onclick = function () {
     clearInterval(timer);
